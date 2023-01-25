0fcd010dabbe3f7106bcfcf10d0bd12f1f04c53b
och-karr <okaa93@gmail.com>
2023-01-26 00:24:47 +0100

    feat: home: add header and show categories

--- src/app/components/home/home.component-module.ts
+++ src/app/components/home/home.component-module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  imports: [],
+  imports: [RouterModule, CommonModule],
   declarations: [HomeComponent],
   providers: [],
   exports: [HomeComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryModel } from '../../models/category.model';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +11,8 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  readonly categoriesList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+
+  constructor(private _categoriesService: CategoriesService) {
+  }
 }
