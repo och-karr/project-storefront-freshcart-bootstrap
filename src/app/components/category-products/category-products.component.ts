0835b8110eead853882f426269837892d2445968
och-karr <okaa93@gmail.com>
2023-01-25 10:55:26 +0100

    feat: create category-products component

---
+++ src/app/components/category-products/category-products.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-category-products',
+  styleUrls: ['./category-products.component.scss'],
+  templateUrl: './category-products.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CategoryProductsComponent {
+}
