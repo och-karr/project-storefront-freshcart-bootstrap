ca66a2b90d1df49b1d6a8a0398a52898901e4ab1
och-karr <okaa93@gmail.com>
2023-01-26 23:09:59 +0100

    fix: move header and footer to app component, change links

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,4 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable } from 'rxjs';
-import { CategoryModel } from '../../models/category.model';
-import { StoreModel } from '../../models/store.model';
-import { CategoriesService } from '../../services/categories.service';
-import { StoresService } from '../../services/stores.service';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +8,5 @@ import { StoresService } from '../../services/stores.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-  readonly categoriesList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
-  readonly storesList$: Observable<StoreModel[]> = this._storesService.getAllStores();
 
-  constructor(private _categoriesService: CategoriesService, private _storesService: StoresService) {
-  }
 }
