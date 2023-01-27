b93604f6ae6ee134295ed68413fe0336e0d7ca96
och-karr <okaa93@gmail.com>
2023-01-27 14:15:15 +0100

    feat: home: add stores section and display stores cards

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,10 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {combineLatest, map, Observable} from "rxjs";
+import {StoreModel} from "../../models/store.model";
+import {StoresService} from "../../services/stores.service";
+import {StoreTagsService} from "../../services/store-tags.service";
+import {StoreTagModel} from "../../models/store-tag.model";
+import {StoreWithTagsNamesQueryModel} from "../../query-models/store-with-tags-names.query-model";
 
 @Component({
   selector: 'app-home',
@@ -8,5 +14,37 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  readonly stores$: Observable<StoreModel[]> = this._storesService.getAllStores();
+  readonly storeTags$: Observable<StoreTagModel[]> = this._storeTagsService.getAllStoreTags();
 
+  readonly storesQuantity = this.stores$.subscribe(
+    stores => {
+      console.log(stores.length)
+      return stores.length;
+    }
+  );
+
+  readonly storesList$: Observable<StoreWithTagsNamesQueryModel[]> = combineLatest([
+    this.stores$,
+    this.storeTags$
+  ]).pipe(
+    map(([stores, tags]: [StoreModel[], StoreTagModel[]]) =>
+      this._mapToStoresWithTagsNames(stores, tags)
+  ));
+
+  constructor(private _storesService: StoresService,private _storeTagsService: StoreTagsService) {
+  }
+
+  private _mapToStoresWithTagsNames(stores: StoreModel[], tags: StoreTagModel[]): StoreWithTagsNamesQueryModel[] {
+    const tagsMap = tags.reduce((a, c) => ({...a, [c.id]: c}), {}) as Record<string, StoreTagModel>
+
+    return stores.map((store) => ({
+        distanceInMeters: store.distanceInMeters,
+        id: store.id,
+        logoUrl: store.logoUrl,
+        name: store.name,
+        tagNames: store.tagIds.map((id) => tagsMap[id].name)
+      })
+    )
+  }
 }
