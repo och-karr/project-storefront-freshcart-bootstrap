bca24fcacd6038fa028ee56f880d56a528c06453
och-karr <okaa93@gmail.com>
2023-01-28 21:59:01 +0100

    feat: store: display products by selected store and input value

--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {Observable, switchMap} from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import {combineLatest, map, Observable, shareReplay, startWith} from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { StoreModel } from '../../models/store.model';
+import { ProductModel } from '../../models/product.model';
 import { StoresService } from '../../services/stores.service';
-import {ActivatedRoute} from "@angular/router";
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-store-products',
@@ -18,6 +22,24 @@ export class StoreProductsComponent {
     })
   );
 
-  constructor(private _storesService: StoresService, private _activatedRoute: ActivatedRoute) {
+  readonly searchForm: FormGroup = new FormGroup({ search: new FormControl('') });
+
+  readonly searchVal$: Observable<string> = this.searchForm.controls['search'].valueChanges.pipe(
+    startWith(''), shareReplay(1)
+  );
+  readonly currentStoreProducts$: Observable<ProductModel[]> = combineLatest([
+    this.searchVal$,
+    this._productsService.getAllProducts(),
+    this.currentStore$.pipe(shareReplay(1))
+  ]).pipe(
+    map(([searchVal, products, store]: [any, ProductModel[], StoreModel]) => {
+      console.log(store.id)
+      return products
+        .filter(product => product.storeIds.includes(store.id))
+        .filter(product => product.name.includes(searchVal))
+    })
+  );
+
+  constructor(private _storesService: StoresService, private _activatedRoute: ActivatedRoute, private _productsService: ProductsService) {
   }
 }
