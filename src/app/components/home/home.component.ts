95295af7b3e8be477de408f568d9e7562010ef37
och-karr <okaa93@gmail.com>
2023-01-28 16:31:46 +0100

    feat: home: create fruitsList and snacksList variables in component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {map, Observable} from "rxjs";
+import {ProductModel} from "../../models/product.model";
+import {ProductsService} from "../../services/products.service";
+import {CategoriesService} from "../../services/categories.service";
 
 @Component({
   selector: 'app-home',
@@ -8,5 +12,26 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  readonly productList$: Observable<ProductModel[]> = this._productsService.getAllProducts();
 
+  readonly fruitsList$: Observable<ProductModel[]> = this.productList$.pipe(
+    map((product) => {
+      return this.getProductsByCategoryId(product, '5');
+    })
+  )
+
+  readonly snacksList$: Observable<ProductModel[]> = this.productList$.pipe(
+    map((product) => {
+      return this.getProductsByCategoryId(product, '2');
+    })
+  )
+
+  getProductsByCategoryId(product: ProductModel[], id: string) {
+    return product.filter((product) => product.categoryId === id)
+      .sort((a, b) => b.featureValue - a.featureValue)
+      .slice(0, 5);
+  }
+
+  constructor(private _productsService: ProductsService, private _categoriesService: CategoriesService) {
+  }
 }
