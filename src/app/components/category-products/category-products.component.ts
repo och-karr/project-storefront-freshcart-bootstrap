ddfdddb8641e5c6683b84c124de30d759ed9b5ba
och-karr <okaa93@gmail.com>
2023-01-28 23:31:09 +0100

    feat and style: display category list and category name and style it

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {Observable, switchMap} from "rxjs";
+import {CategoryModel} from "../../models/category.model";
+import {CategoriesService} from "../../services/categories.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-category-products',
@@ -8,4 +12,14 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoryProductsComponent {
+
+  readonly categoriesList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+  readonly currentCategory$: Observable<CategoryModel> = this._activatedRoute.params.pipe(
+    switchMap((data) => {
+      return this._categoriesService.getOneCategory(+data['categoryId'])
+    })
+  );
+
+  constructor(private _categoriesService: CategoriesService, private _activatedRoute: ActivatedRoute) {
+  }
 }
