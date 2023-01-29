47ce8acd2a6f9eabbe207734be5b5ab0963117f4
och-karr <okaa93@gmail.com>
2023-01-29 23:07:32 +0100

    feat: category: add working functions on change limit and change page

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import {Observable, combineLatest, of, startWith} from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, combineLatest, of, startWith } from 'rxjs';
+import {  map, switchMap } from 'rxjs/operators';
 import { CategoryModel } from '../../models/category.model';
 import { ProductModel } from '../../models/product.model';
 import { CategoriesService } from '../../services/categories.service';
@@ -44,6 +44,12 @@ export class CategoryProductsComponent {
 
   readonly sortingOption: FormControl = new FormControl();
 
+  private _currentLimitSubject: BehaviorSubject<number> = new BehaviorSubject<number>(2);
+  public currentLimit$: Observable<number> = this._currentLimitSubject.asObservable();
+
+  private _currentPageSubject: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+  public currentPage$: Observable<number> = this._currentPageSubject.asObservable();
+
   readonly productsList$: Observable<ProductModel[]> = combineLatest([
     this._productsService.getAllProducts(),
     this.currentCategory$,
@@ -71,17 +77,35 @@ export class CategoryProductsComponent {
     })
   );
 
+  readonly limits$: Observable<number[]> = of([1, 2, 3]);
+  readonly pages$: Observable<number[]> = combineLatest([
+    this.productsList$,
+    this.currentLimit$
+  ]).pipe(
+    map(([products, limit]) => {
+      return Array.from({ length: Math.ceil(products.length / limit) }, (_, i) => i + 1)
+    })
+  )
+
+  readonly paginatedProductsList$: Observable<ProductModel[]> = combineLatest([
+    this.productsList$,
+    this.currentLimit$,
+    this.currentPage$
+  ]).pipe(
+    map(([products, limit, page]) => products.slice(((page - 1) * limit), ((page-1) * limit + limit)))
+  )
+
   countStars(ratingVal: number) {
     let starArray = [];
-    for(let i = 0; i < Math.floor(ratingVal); i++) {
+    for (let i = 0; i < Math.floor(ratingVal); i++) {
       starArray.push(1);
     }
 
-    if(ratingVal > Math.floor(ratingVal)) {
+    if (ratingVal > Math.floor(ratingVal)) {
       starArray.push(.5);
     }
 
-    while(starArray.length < 5) {
+    while (starArray.length < 5) {
       starArray.push(0);
     }
 
@@ -91,4 +115,12 @@ export class CategoryProductsComponent {
   constructor(private _categoriesService: CategoriesService, private _router: Router, private _activatedRoute: ActivatedRoute, private _productsService: ProductsService) {
   }
 
+  changeLimit(item: number) {
+    console.log(item)
+    this._currentLimitSubject.next(item);
+  }
+
+  changePage(item: number) {
+    this._currentPageSubject.next(item);
+  }
 }
