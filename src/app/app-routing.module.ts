7302abf8e73a971e7f4cb639ccf009ebcba8c248
och-karr <okaa93@gmail.com>
2023-02-03 16:46:11 +0100

    feat: add routing - product detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,28 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {CategoryProductsComponent} from './components/category-products/category-products.component';
 import {StoreProductsComponent} from './components/store-products/store-products.component';
+import {ProductDetailComponent} from './components/product-detail/product-detail.component';
 import {HomeComponentModule} from './components/home/home.component-module';
 import {CategoryProductsComponentModule} from './components/category-products/category-products.component-module';
 import {StoreProductsComponentModule} from './components/store-products/store-products.component-module';
+import {ProductDetailComponentModule} from './components/product-detail/product-detail.component-module';
 
 const routes: Routes = [{
-    path: '',
-    component: HomeComponent
-  }, {
-    path: 'categories/:categoryId',
-    component: CategoryProductsComponent
-  }, {
-    path: 'stores/:storeId',
-    component: StoreProductsComponent
-  }];
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'categories/:categoryId',
+  component: CategoryProductsComponent
+}, {
+  path: 'stores/:storeId',
+  component: StoreProductsComponent
+}, {
+  path: 'products/:productId',
+  component: ProductDetailComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), HomeComponentModule, CategoryProductsComponentModule, StoreProductsComponentModule],
+  imports: [RouterModule.forRoot(routes), HomeComponentModule, CategoryProductsComponentModule, StoreProductsComponentModule, ProductDetailComponentModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
